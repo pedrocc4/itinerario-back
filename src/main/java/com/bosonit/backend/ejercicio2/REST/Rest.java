52fd373a50d382d853b5041ebba1e4be93f3727a
Pedro Corpas Cuesta <pedro.corpas@bosonit.com>
2022-03-11 09:22:14 +0100

    Método POST terminado

--- src/main/java/com/bosonit/backend/ejercicio2/REST/Rest.java
+++ src/main/java/com/bosonit/backend/ejercicio2/REST/Rest.java
@@ -1,11 +1,9 @@
 package com.bosonit.backend.ejercicio2.REST;
 
+import com.bosonit.backend.ejercicio2.entidades.Persona;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
@@ -19,5 +17,14 @@ public class Rest {
         log.info("Nombre: " + nombre);
         String hola = "Hola: " + nombre;
         return ResponseEntity.ok(hola);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("useradd")
+    public ResponseEntity<Persona> crearPersona(
+            @RequestBody Persona persona) {     // posteriormente usaremos DTO
+        log.info("Recibida: " + persona);
+        persona.setEdad(persona.getEdad() + 1);
+        return ResponseEntity.status(HttpStatus.CREATED).body(persona);
     }
 }
