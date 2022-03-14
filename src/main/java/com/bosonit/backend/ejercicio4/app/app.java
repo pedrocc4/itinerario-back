b635652ec6e47a5106cf043d5d55d2204e4e2eee
Pedro Corpas Cuesta <pedro.corpas@bosonit.com>
2022-03-14 10:41:09 +0100

    Ejercicio Logging - implementada la escritrura en un fichero.log

--- src/main/java/com/bosonit/backend/ejercicio4/app/app.java
+++ src/main/java/com/bosonit/backend/ejercicio4/app/app.java
@@ -1,9 +1,11 @@
 package com.bosonit.backend.ejercicio4.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@Slf4j
 @SpringBootApplication(scanBasePackages = {
         "com.bosonit.backend.ejercicio4.REST",
         "com.bosonit.backend.ejercicio4.configuration",
@@ -12,7 +14,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 public class app {
 
     public static void main(String[] args) {
-        SpringApplication.run(app.class, args);
-    }
 
+        SpringApplication.run(app.class, args);
+        log.error("Esto es una prueba de error");
+    }
 }
