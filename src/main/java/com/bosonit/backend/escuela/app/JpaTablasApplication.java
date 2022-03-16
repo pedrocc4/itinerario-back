093c5ed3a4d3840ddca74b0a53766a1dc550c622
Pedro Corpas Cuesta <pedro.corpas@bosonit.com>
2022-03-16 10:00:17 +0100

    Nueva rama (relación varias tablas)

---
+++ src/main/java/com/bosonit/backend/escuela/app/JpaTablasApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.backend.escuela.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTablasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaTablasApplication.class, args);
+    }
+
+}
